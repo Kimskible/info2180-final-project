158e48d36958dca0cae416c9333901f7d394e0d4
caribbeanyute <smithcleon@gmail.com>
2019-12-02 01:35:48 +0000

    Fixing login

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 
 require_once('./util/db.php');
 
- if(true)
+ if($_SERVER["REQUEST_METHOD"] == "POST")
  {
 
    $email 	= $_POST['email'];
@@ -33,3 +33,43 @@ require_once('./util/db.php');
  }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="login.css">
+  <title>BugMe Issue Tracker</title>
+</head>
+
+<body>
+  <div class="header">
+  </div>
+  <div class="login">
+    <form  method="POST" action="login.php">
+      <div class="imgcontainer">
+        <img src="avatar.png" alt="Avatar" class="avatar">
+      </div>
+  
+      <div class="container">
+        <label for="email"><b> Email </b></label>
+        <input type="email" placeholder="Enter your email" name="email" required>
+    
+        <label for="psw"><b> Password </b></label>
+        <input type="password" placeholder="Enter your password" name="psw" required>
+  
+        <button type="submit">LOGIN</button>
+      </div>
+      
+  
+    </form>
+
+  </div>
+</body>
+
+</html>
+
+
